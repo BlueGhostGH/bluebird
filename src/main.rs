7581e96624572923cd12f937cf107b00df16790f
George Dragomir <george.dragomir.x@gmail.com>
2022-11-17 01:31:26 +0200

    Add a database connection

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,24 @@
-use std::net::SocketAddr;
+use std::{net::SocketAddr, time::Duration};
 
-use axum::{routing::get, Router};
+use axum::{extract::State, http::StatusCode, routing::get, Router};
+use sqlx::postgres::{PgPool, PgPoolOptions};
 
 #[tokio::main]
 async fn main()
 {
     tracing_subscriber::fmt::init();
 
-    let app = Router::new().route("/", get(root));
+    let db_connection_str = std::env::var("DATABASE_URL")
+        .unwrap_or_else(|_| String::from("postgres://postgres:postgres@localhost/bluebird"));
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .acquire_timeout(Duration::from_secs(3))
+        .connect(&db_connection_str)
+        .await
+        .expect("Cannot connect to database");
+
+    let app = Router::with_state(pool).route("/", get(root));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
@@ -18,7 +29,20 @@ async fn main()
         .unwrap();
 }
 
-async fn root() -> &'static str
+async fn root(State(pool): State<PgPool>) -> Result<String, (StatusCode, String)>
 {
-    "Hello, world!"
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(256_i64)
+        .fetch_one(&pool)
+        .await
+        .map_err(internal_err)?;
+
+    Ok(row.0.to_string())
+}
+
+fn internal_err<E>(err: E) -> (StatusCode, String)
+where
+    E: std::error::Error,
+{
+    (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
 }
