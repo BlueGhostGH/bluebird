484896147964372def9763d610378204e901be7b
George Dragomir <george.dragomir.x@gmail.com>
2022-11-16 14:46:48 +0200

    Set up a basic Axum server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
-fn main()
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main()
 {
-    println!("Hello, world!");
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new().route("/", get(root));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str
+{
+    "Hello, world!"
 }
